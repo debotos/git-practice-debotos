90d2aed293971a566cdd8873422bdfa5e3979e10
debotos <debotosdas@gmail.com>
2026-04-30 16:48:47 +0600

    Added basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from datetime import date
 
+from utils import add, subtract
+
 def main():
     print('Debotos')
     print(date.today())
+    print(add(5, 3))
+    print(subtract(5, 3))
 
 if __name__ == "__main__":
     main()

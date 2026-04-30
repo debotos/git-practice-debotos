1c95579a40c117c7c4d69d79f2bd6a3b8e9ce840
debotos <debotosdas@gmail.com>
2026-04-30 16:43:27 +0600

    Initial commit: project structure and basic program

---
+++ README.md
@@ -0,0 +1,7 @@
+# GIT PRACTICE
+
+## Debotos
+
+### What this project does?
+
+This is just a git practice project.
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+def main():
+    print('Debotos')
+    print(date.today())
+
+if __name__ == "__main__":
+    main()
